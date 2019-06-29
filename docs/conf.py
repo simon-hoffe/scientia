1dfd0670cd3c2771d6fec2c251017a01abcabba8
Simon Hoffe <simon.hoffe@ieee.org>
2019-06-29 09:34:50 +0200

    Added pygments_style = 'default' to conf.py, per instructions at https://github.com/readthedocs/sphinx_rtd_theme/issues/633

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+pygments_style = 'default'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
